46ebba02367f28d1dfc07c6025679d6bc57b7ea7
Jekaterina Salina <jekaterinasalina@gmail.com>
2019-04-01 19:29:54 +0300

    mappings
    Took 23 minutes

--- src/main/java/io/codelex/studentsystem/PublicController.java
+++ src/main/java/io/codelex/studentsystem/PublicController.java
@@ -1,9 +1,33 @@
 package io.codelex.studentsystem;
 
+import io.codelex.studentsystem.api.Group;
+import io.codelex.studentsystem.api.Student;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class PublicController {
+    
+    @GetMapping("/groups")
+    public List<Group> findAllGroups(){
+        return null;
+    }
+    
+    @GetMapping("/groups/{id}")
+    public Group findGroup(){
+        return null;
+    }
+    
+    @GetMapping("/groups/{id}/students")
+    public List<Student> findAllStudents(){
+        return null;
+    }
+    @GetMapping("groups/{id}/students/{id}")
+    public Student findStudent(){
+        return null;
+    }
 }
