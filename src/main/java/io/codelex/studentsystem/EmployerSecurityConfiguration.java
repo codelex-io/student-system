e03b35c863ccb1672849d251e290e3030bc6bb27
Matiss <m.tolocko@gmail.com>
2019-04-03 21:04:29 +0300

    Authentication first commit

---
+++ src/main/java/io/codelex/studentsystem/EmployerSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package io.codelex.studentsystem;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Order(100)
+@Configuration
+class EmployerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/api/**")
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/sign-in", "/api/register").permitAll()
+                .anyRequest().hasRole("EMPLOYER");
+    }
+}
