17087a536e955cf5c0dfd403ec261f0f88e77c40
Janis Libeks <lybexx@gmail.com>
2019-04-11 10:33:22 +0300

    added groupController test mockBean InstructorService
    Took 36 minutes

--- src/main/java/io/codelex/studentsystem/repository/recordRepository/EmployerRecordRepository.java
+++ src/main/java/io/codelex/studentsystem/repository/recordRepository/EmployerRecordRepository.java
@@ -2,6 +2,12 @@ package io.codelex.studentsystem.repository.recordRepository;
 
 import io.codelex.studentsystem.repository.model.EmployerRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployerRecordRepository extends JpaRepository<EmployerRecord, Long> {
+    
+    @Query("select count(employer) > 0 from EmployerRecord employer "
+            + "where employer.id = :id")
+    boolean isEmployerIdPresent(@Param("id") Long id);
 }
