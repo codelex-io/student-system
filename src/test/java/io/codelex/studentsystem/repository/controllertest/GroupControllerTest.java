0efc177cb5aa2db7fae5d29ea56bb86b094be2f1
Matiss <m.tolocko@gmail.com>
2019-04-13 15:51:53 +0300

    json fix

--- src/test/java/io/codelex/studentsystem/repository/controllertest/GroupControllerTest.java
+++ src/test/java/io/codelex/studentsystem/repository/controllertest/GroupControllerTest.java
@@ -8,6 +8,7 @@ import io.codelex.studentsystem.service.InstructorService;
 import io.codelex.studentsystem.service.StudentService;
 import io.codelex.studentsystem.service.TopicsService;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDate;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -50,12 +55,7 @@ class GroupControllerTest {
     @Test
     void should_add_group_and_give_200_response() throws Exception {
         //given
-        AddGroup request = new AddGroup("Group",
-                defaultDate,
-                defaultDate.plusDays(1),
-                defaultDate.plusDays(2)
-        );
-        String json = MAPPER.writeValueAsString(request);
+        byte[] json = getRequest("/requests/addGroupCorrect");
         //expected
         mockMvc.perform(
                 put("/internal-api/groups")
@@ -67,15 +67,16 @@ class GroupControllerTest {
                 );
     }
 
+    private byte[] getRequest(String s) throws IOException {
+        File file = ResourceUtils.getFile(this.getClass().getResource(s));
+        Assertions.assertTrue(file.exists());
+        return Files.readAllBytes(file.toPath());
+    }
+
     @Test
     void should_not_add_group_and_give_400_response_if_fields_null() throws Exception {
         //given
-        AddGroup request = new AddGroup("Group",
-                null,
-                defaultDate.plusDays(1),
-                defaultDate.plusDays(2)
-        );
-        String json = MAPPER.writeValueAsString(request);
+        byte[] json = getRequest("/requests/addGroupWithNull");
         //expected
         mockMvc.perform(
                 put("/internal-api/groups")
@@ -90,12 +91,7 @@ class GroupControllerTest {
     @Test
     void should_not_add_group_and_give_400_response_if_contains_empty_field() throws Exception {
         //given
-        AddGroup request = new AddGroup("",
-                defaultDate,
-                defaultDate.plusDays(1),
-                defaultDate.plusDays(2)
-        );
-        String json = MAPPER.writeValueAsString(request);
+        byte[] json = getRequest("/requests/addGroupWithEmptyField");
         //expected
         mockMvc.perform(
                 put("/internal-api/groups")
@@ -110,8 +106,7 @@ class GroupControllerTest {
     @Test
     void should_not_find_group_by_id_if_no_such_id_and_give_400_response() throws Exception {
         //given
-        AddGroup request = addGroupRequest();
-        String json = MAPPER.writeValueAsString(request);
+        byte[] json = getRequest("/requests/addGroupCorrect");
         //expected
         mockMvc.perform(
                 get("/internal-api/groups/222")
@@ -127,8 +122,7 @@ class GroupControllerTest {
     @Test
     void should_return_400_response_if_no_such_id_to_delete() throws Exception {
         //given
-        AddGroup request = addGroupRequest();
-        String json = MAPPER.writeValueAsString(request);
+        byte[] json = getRequest("/requests/addGroupCorrect");
         //expected
         mockMvc.perform(
                 delete("/internal-api/groups/1")
@@ -138,14 +132,5 @@ class GroupControllerTest {
                 .andDo(print())
                 .andExpect(status().isBadRequest()
                 );
-    }
-
-    @NotNull
-    private AddGroup addGroupRequest() {
-        return new AddGroup("group",
-                defaultDate,
-                defaultDate.plusDays(1),
-                defaultDate.plusDays(2)
-        );
     }
 }
